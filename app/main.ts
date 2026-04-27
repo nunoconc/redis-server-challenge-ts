45d2bd4b9e4ba80a20a94d81b33232bd73260a85
nunoconc <nmpconceicao@gmail.com>
2026-04-27 16:51:02 +0100

    feat: add PONG output response

--- app/main.ts
+++ app/main.ts
@@ -4,7 +4,11 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 // Uncomment the code below to pass the first stage
-const server: net.Server = net.createServer((connection: net.Socket) => {// Handle connection
+const server: net.Server = net.createServer((connection: net.Socket) => {
+    // Handle connection
+    connection.on("data", (data: Buffer) => {
+        connection.write("+PONG\r\n");
+    })
 });
 
 server.listen(6379, "127.0.0.1");
